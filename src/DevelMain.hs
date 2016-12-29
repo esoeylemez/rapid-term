d32dffa5b4f51738c12a0c9c30526fd6ee84f701
Ertugrul Söylemez <esz@posteo.de>
2016-12-30 00:20:50 +0100

    Add DevelMain

---
+++ src/DevelMain.hs
@@ -0,0 +1,25 @@
+-- |
+-- Copyright:  (c) 2016 Ertugrul Söylemez
+-- License:    BSD3
+-- Maintainer: Ertugrul Söylemez <esz@posteo.de>
+
+module DevelMain (update) where
+
+import Control.Concurrent
+import Control.Exception
+import Rapid
+import Rapid.Term
+import System.IO
+
+
+update :: IO ()
+update =
+    rapid 0 $ \r -> do
+        t <- createRef r "term-ref" newTermRef
+        start r "term" (runTerm (urxvtAt "./urxvt") t)
+        restart r "test-app" $
+            terminal t $ \h ->
+                let putLn = hPutStrLn h in
+                bracket_ (putLn "--- APPLICATION START ---")
+                         (putLn "--- APPLICATION STOP ---")
+                         (threadDelay 10000000)
